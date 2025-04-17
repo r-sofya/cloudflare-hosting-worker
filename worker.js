d508a68b4fa687ea469692ded10d0b48f39d7c84
Rani Sofya <rani.sofya@trulycustom.ca>
2025-04-16 22:03:57 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,11 +2,11 @@ import { createTemplateWorker } from 'https://libraries-cmt.pages.dev/hosting-sc
 
 export default {
   async fetch(request, env, ctx) {
-    const CACHE_VERSION_NAME = "CACHE_VERSION_PROJECT1";
+    const CACHE_VERSION_NAME = "Truly Custom Cache";
 
     const config = {
-      TARGET_URL: env.TARGET_URL,
-      CANONICAL_DOMAIN: env.CANONICAL_DOMAIN,
+      TARGET_URL: env.TARGET_URL || "https://truly-custom.webflow.io",
+      CANONICAL_DOMAIN: env.CANONICAL_DOMAIN || "https://www.example.com",
       CACHE_VERSION: env[CACHE_VERSION_NAME]
     };
 
